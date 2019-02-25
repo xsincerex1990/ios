98f3d8384e5972be4a5e55d02ad0458b3839a2e6
xsincerex1990 <xsincerex1990@gmail.com>
2019-02-25 16:12:50 -0500

    Las vegas dice app

---
+++ swift_practice/dicee/dicee/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  dicee
+//
+//  Created by Joel alexis on 2/25/19.
+//  Copyright © 2019 Joel alexis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var randomDice1: Int = 0
+    var randomDice2: Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateImg()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateImg()
+    }
+    
+    func updateImg() {
+        
+        randomDice1 = Int.random(in: 1 ... 6)
+        randomDice2 = Int.random(in: 1 ... 6)
+        
+        diceImageView1.image = UIImage(named: "dice" + String(randomDice1))
+        diceImageView2.image = UIImage(named: "dice" + String(randomDice2))
+    }
+    
+
+}
+
