d7fda75ccc2377d580fc8a2e27d468fc243e8ef5
xsincerex1990 <xsincerex1990@gmail.com>
2019-03-01 19:00:01 -0500

    calculator

---
+++ swift_practice/stack_layout/stack_layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stack_layout
+//
+//  Created by Joel alexis on 3/1/19.
+//  Copyright © 2019 Joel alexis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
